c450c77328bac3025f2b4fe37ada3d15d29ce9a6
Soham Tamboli <sohamtamboli@gmail.com>
2020-09-17 01:32:49 +0530

    basic navbar component

---
+++ pages/landing.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <Navbar />
+  </div>
+</template>
+
+<script>
+import Navbar from '../components/Navbar'
+export default {
+  name: 'Landing',
+  components: {
+    Navbar,
+  },
+}
+</script>
+
+<style lang="scss" scoped></style>
