5ac16d480d88216b9f1592b3159f0abbbf6c19f7
Pruthvi Shetty <pruthvishetty5656@gmail.com>
2020-09-25 23:13:47 +0530

    added tests for Logo File for reference

--- jest.config.js
+++ jest.config.js
@@ -1,18 +1,23 @@
+
 module.exports = {
+  // tell Jest to handle `*.vue` files
+  moduleFileExtensions: ["js", "json", "vue", "ts"],
+  watchman: false,
   moduleNameMapper: {
-    '^@/(.*)$': '<rootDir>/$1',
-    '^~/(.*)$': '<rootDir>/$1',
-    '^vue$': 'vue/dist/vue.common.js',
+    "^~/(.*)$": "<rootDir>/$1",
+    "^~~/(.*)$": "<rootDir>/$1",
+    "^@/(.*)$": "<rootDir>/$1"
   },
-  moduleFileExtensions: ['ts', 'js', 'vue', 'json'],
   transform: {
-    '^.+\\.ts$': 'ts-jest',
-    '^.+\\.js$': 'babel-jest',
-    '.*\\.(vue)$': 'vue-jest',
+    // process js with `babel-jest`
+    "^.+\\.js$": "<rootDir>/node_modules/babel-jest",
+    // process `*.vue` files with `vue-jest`
+    ".*\\.(vue)$": "<rootDir>/node_modules/vue-jest"
   },
+  snapshotSerializers: ["<rootDir>/node_modules/jest-serializer-vue"],
   collectCoverage: true,
   collectCoverageFrom: [
-    '<rootDir>/components/**/*.vue',
-    '<rootDir>/pages/**/*.vue',
-  ],
-}
+    "<rootDir>/components/**/*.vue",
+    "<rootDir>/pages/*.vue"
+  ]
+};
